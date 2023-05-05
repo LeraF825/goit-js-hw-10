e778c159eb438c936c14fd1cd4b576efbd2f715d
LeraF825 <fertikovalera@gmail.com>
2023-05-05 17:17:46 +0300

    update task

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+  const END_POINT = '/name';
+  const PARAMS = new URLSearchParams({
+    name,
+    capital,
+    population,
+    flags,
+    languages,
+  });
+
+  const url = BASE_URL + END_POINT + PARAMS;
+  
+  return fetch(url).then(res=>res.json());
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+
+const searchForm = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+searchForm.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
+
+function onInput(e){
+const symbol = e.target.value.trim();
+}
